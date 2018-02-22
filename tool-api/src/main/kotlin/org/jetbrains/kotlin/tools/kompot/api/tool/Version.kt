8449b6ed25935c7e54ddc083c898b6ee9dc6959f
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-02-22 13:06:22 +0300

    Support using VersionHandler as VersionCompareHandler

--- tool-api/src/main/kotlin/org/jetbrains/kotlin/tools/kompot/api/tool/Version.kt
+++ tool-api/src/main/kotlin/org/jetbrains/kotlin/tools/kompot/api/tool/Version.kt
@@ -2,9 +2,12 @@ package org.jetbrains.kotlin.tools.kompot.api.tool
 
 import java.io.Serializable
 
-interface VersionHandler : Serializable {
+interface VersionHandler : VersionCompareHandler, Serializable {
     fun plus(t: Version?, other: Version?): Version?
     fun contains(t: Version?, other: Version?): Boolean
+    override fun isSubset(a: Version?, b: Version?): Boolean {
+        return a != b && contains(b, a)
+    }
 }
 
 
